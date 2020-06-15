78393dd7bca5bf024aa6807135e07b27d9374120
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-15 16:54:56 -0300

    remove node_modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,12 @@
-const express from 'express';
+const express = require('express');
 
 const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        name: 'Jorge',
+        email: 'jorgel@cbm.sc.gov.br'
+    })
+});
 
 app.listen(3333);
