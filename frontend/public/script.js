e684755b18840205472c9e9ea81d29db71031bc0
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-16 16:25:55 -0300

    Começando com React

---
+++ frontend/public/script.js
@@ -0,0 +1,8 @@
+
+// Babel : Converter (Transpilar) o código mais atualizado do react 
+//   para um código que o borwaser entenda.
+// Webpack: Para cada tipo de arquivo (css, img, png, js) vai converter de uma maneira.
+//
+//
+// Loaders: babel-loader, css-loader, image-loader
+
