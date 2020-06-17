a04fac3958960e1d884439065291585c96c2d58a
Jorge Dias <jorgediasdsg@gmail.com>
2020-06-16 23:14:28 -0300

    Configurando React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { userState, useState } from 'react';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
-import { unmountComponentAtNode } from 'react-dom';
 
 // Componente
 // Propriedade
 // Estado
+
+// useState retorna arrey com 2 posições
+// 1 posição retorna a variavel com o valor atual
+// 2 posição é uma função para alterar seu valor.
+
 function App () {
+
+    const [projects, setProjects] = useState(['Desenvolvimento de app','Front-end web']);
+
+    function handleAddProject(){
+         //Conceito de imutabilidade
+        // Spread tras tudo.
+        setProjects([...projects, `Novo projeto ${Date.now()}`])
+    }
+
     return (
-    <>
-        <Header title="Homepage">
+        <>
+            <Header title="Homepage" />
+
+            <img width={300} src={backgroundImage}/>
+
             <ul>
-                <li>HomePage</li>
-                <li>Projects</li>
+                {projects.map(project => <li key={project}>{project}</li>)}
             </ul>
-        </Header>
-        <Header title="Projects">
-            <ul>
-                <li>HomePage</li>
-                <li>Projects</li>
-                <li>Login</li>
-            </ul>
-        </Header>
-    </>
-    )
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
+        </>
+    );
 }
 
 export default App;
